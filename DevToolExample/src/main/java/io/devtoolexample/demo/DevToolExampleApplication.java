d7a6b15e005bda69d29accaab9551dff59d33995
user <user@LAPTOP-R2O30Q36>
2018-08-26 13:06:58 +0530

    Initial Commit

---
+++ DevToolExample/src/main/java/io/devtoolexample/demo/DevToolExampleApplication.java
@@ -0,0 +1,12 @@
+package io.devtoolexample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolExampleApplication.class, args);
+	}
+}
